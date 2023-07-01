eca3ea2db56166d68983761995c85b6b649c3a87
AlexLP1010 <lopezalejandro10102001@gamil.com>
2023-06-30 19:37:48 -0600

    Simple list load

--- src/main.js
+++ src/main.js
@@ -1,34 +1,15 @@
+// DOM Objects
 const pokeList = document.getElementById('pokeList')
 const pokeImage = document.getElementById('pokeImage')
 const pokemonId = document.getElementById('pokemonId')
 const pokeName = document.getElementById('pokeName')
 const pokeTypes = document.getElementById('pokeTypes')
-const loadMore = document.getElementById('loadMore')
+const loadingElement = document.getElementById('loadingElement')
 
-function getPokemonList() {
-  let  url = 'https://pokeapi.co/api/v2/pokemon/'
-
-  const next = () => {
-    fetch(url)
-      .then(res => res.json())
-      .then(data => {
-        data.results.forEach(pokemon => {
-          const newPokemon = document.createElement('button')
-          newPokemon.type = 'button'
-          newPokemon.innerText = pokemon.name
-          newPokemon.addEventListener('click', () => {
-            selectPokemon(pokemon.url)
-          })
-          pokeList.appendChild(newPokemon)
-        })
-        url = data.next
-      })
-      .catch(err => console.error(err))
-  }
-
-  return next
-}
-
+/**
+ * 
+ * @param {string} url 
+ */
 function selectPokemon(url) {
   fetch(url)
     .then(res => res.json())
@@ -45,10 +26,54 @@ function selectPokemon(url) {
     })
 }
 
+function getPokemonList() {
+  let  url = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=50'
+
+  const next = () => {
+    if (!url) {
+      observer.disconnect()
+      loadingElement.innerText = 'End'
+      return
+    }
+
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        data.results.forEach(pokemon => {
+          const newPokemon = document.createElement('button')
+          newPokemon.type = 'button'
+          newPokemon.classList.add('pokemon-button')
+          const regex = /(?<!v)\d+/gm
+          const id = regex.exec(pokemon.url)
+          newPokemon.innerText = `${id} ${pokemon.name}`
+          newPokemon.addEventListener('click', () => {
+            selectPokemon(pokemon.url)
+          })
+          pokeList.appendChild(newPokemon)
+        })
+        url = data.next
+      })
+      .catch(err => {
+        observer.disconnect()
+        loadingElement.innerText = 'Error'
+        console.error(err)
+      })
+  }
+
+  return next
+}
+
 const pokemonList = getPokemonList()
 
-loadMore.addEventListener('click', () => {
-  pokemonList()
-})
+const observer = new IntersectionObserver(handleRequestList)
+/**
+ * 
+ * @param {IntersectionObserverEntry[]} entries 
+ */
+function handleRequestList(entries) {
+  const entry = entries[0]
+  if (entry.isIntersecting)
+    pokemonList()
+}
 
-pokemonList()
+observer.observe(loadingElement)
