23f481e3d1d3454326da0a9a0a78718cb4896450
AlexLP1010 <lopezalejandro10102001@gamil.com>
2023-07-06 20:00:10 -0600

    Adding color to pokemon type labels

--- src/main.js
+++ src/main.js
@@ -6,6 +6,29 @@ const pokeName = document.getElementById('pokeName')
 const pokeTypes = document.getElementById('pokeTypes')
 const loadingElement = document.getElementById('loadingElement')
 
+const pokeTypeColors = {
+  normal: '#4f5255',
+  grass: '#62b658',
+  fighting: '#c54069',
+  flying: '#90a9e1',
+  poison: '#ae66cc',
+  ground: '#dd7a4e',
+  rock: '#c2b388',
+  bug: '#8fbf2f',
+  ghost: '#546cac',
+  steel: '#5a8da5',
+  fire: '#ff9b4e',
+  water: '#4e91d4',
+  electric: '#f2d139',
+  psychic: '#fc6f77',
+  ice: '#71cec0',
+  dragon: '#126fc4',
+  dark: '#5e5569',
+  fairy: '#ed93e8',
+  unknown: '#fafafa',
+  shadow: '#2d2726'
+}
+
 /**
  * 
  * @param {string} url 
@@ -20,7 +43,8 @@ function selectPokemon(url) {
       pokeTypes.innerHTML = ''
       pokemon.types.forEach(t => {
         const newType = document.createElement('span')
-        newType.innerText = t.type.name + ' '
+        newType.innerText = t.type.name
+        newType.style = `background-color: ${pokeTypeColors[t.type.name]}; margin-left: 1em;`
         pokeTypes.appendChild(newType)
       })
     })
